51bbef5c5fc396280da2f339033ae835b0cbda5b
sgalal <32255369+chromezh@users.noreply.github.com>
2019-12-09 12:02:10 +0800

    Update to Guangyun database v3.0

--- build/build.py
+++ build/build.py
@@ -14,9 +14,9 @@ def build_small_rhyme():
 	f.write('const small_rhymes=')
 	obj = ['|'.join((小韻, 韻母, 聲母, 開合, str(等))) \
 		for 小韻, 韻母, 聲母, 開合, 等 \
-		in cur.execute('SELECT small_rhyme, of_rhyme, initial, rounding, division FROM full_small_rhymes ORDER BY id;')]
+		in cur.execute('SELECT 小韻, 韻, 母, 開合, 等 FROM 廣韻小韻全 ORDER BY 小韻號;')]
 	json.dump(obj, f, ensure_ascii=False, separators=(',',':'))
-	f.write('.map(x=>{const a=x.split(\'|\');a[4]=a[4]|0;return a;});')  # 用 | 分隔小韻、韻母、聲母、開合、等，且等為數字
+	f.write(".map(x=>{const a=x.split('|');a[4]=a[4]|0;return a;});")  # 用 | 分隔小韻、韻母、聲母、開合、等，且等為數字
 	f.close()
 
 build_small_rhyme()
@@ -26,7 +26,7 @@ def build_char_entity():
 	f.write('const char_entities=(()=>{const d=')
 
 	d = defaultdict(list)
-	for 字, 小韻, 解釋 in cur.execute('SELECT name, of_small_rhyme, explanation FROM core_char_entities WHERE LENGTH(name) = 1;'):
+	for 字, 小韻, 解釋 in cur.execute('SELECT 字頭, 小韻號, 解釋 FROM 廣韻字頭 WHERE length(字頭) = 1;'):
 		d[字].append(str(小韻) + '|' + 解釋)
 	d = {k: '+'.join(vs) for k, vs in d.items()}
 	json.dump(d, f, ensure_ascii=False, separators=(',',':'))
@@ -34,6 +34,9 @@ def build_char_entity():
 	f.close()
 
 build_char_entity()
+
+cur.close()
+conn.close()
 
 def minify_brogue2():
 	with open('build/brogue2.min.js', 'w') as fout:
@@ -51,6 +54,3 @@ def concat_files(l, s):
 	fout.close()
 
 concat_files(('build/map.js', 'build/char_entity.js', 'build/small_rhyme.js', 'build/brogue2.min.js'), 'docs/brogue2.js')
-
-cur.close()
-conn.close()

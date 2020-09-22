c9453437efc049129136ca6a712c75628ebe5a5d
sgalal <sgalal.me@outlook.com>
2020-09-22 16:47:41 +0800

    Pin qieyun-sqlite version 20200921

--- build/A_prepare_data.sh
+++ build/A_prepare_data.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 mkdir -p cache
-wget -nc -P cache https://github.com/nk2028/qieyun-sqlite/releases/latest/download/qieyun.sqlite3
+wget -nc -P cache https://github.com/nk2028/qieyun-sqlite/releases/download/20200921/qieyun.sqlite3
 wget -nc -P cache https://raw.githubusercontent.com/BYVoid/ytenx/master/ytenx/sync/kyonh/YonhMiuk.txt
 wget -nc -P cache https://raw.githubusercontent.com/BYVoid/ytenx/master/ytenx/sync/kyonh/YonhGheh.txt
 mkdir -p output

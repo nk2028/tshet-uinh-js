6107340c5fe1dc84cfccb9281d0d662d9447fef1
sgalal <sgalal.me@outlook.com>
2020-09-24 11:27:33 +0800

    Concat 3 files

--- build/C_concat_files.sh
+++ build/C_concat_files.sh
@@ -7,9 +7,7 @@ cat build/D_header.js >> index.js
 cat output/E_壓縮的字頭資料.js >> index.js
 cat output/F_壓縮的小韻資料.js >> index.js
 cat output/G_map1.js >> index.js
-cat build/H_map.js >> index.js
-cat build/I_unpack_data.js >> index.js
-cat build/J_core.js >> index.js
+cat build/H_core.js >> index.js
 
 echo 'return _qieyunExports;
 })();

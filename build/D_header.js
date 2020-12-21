a709399540949da7e10d7d3d9b570d407549913c
Ayaka Mikazuki <ayaka@mail.shn.hk>
2020-12-21 16:02:44 +0800

    Bump version to 0.7.6

--- build/D_header.js
+++ build/D_header.js
@@ -25,7 +25,7 @@
  * Browser:
  *
  * ```html
- * <script src="https://cdn.jsdelivr.net/npm/qieyun@0.7.5"></script>
+ * <script src="https://cdn.jsdelivr.net/npm/qieyun@0.7.6"></script>
  * ```
  *
  * Node.js:

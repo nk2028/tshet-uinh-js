5794317a5124e365d75e838167be157a2c8a2b8f
sgalal <sgalal.me@outlook.com>
2021-01-05 23:32:58 +0800

    Update to https://github.com/nk2028/qieyun-data/commit/7e2aa4a497bcb38caea6b803dbae7d706851d74f

--- src/音韻編碼.js
+++ src/音韻編碼.js
@@ -5,6 +5,8 @@ const 所有等 = '一二三四';
 const 所有重紐 = 'AB';
 const 所有韻 = '東冬鍾江支脂之微魚虞模齊祭泰佳皆夬灰咍廢眞臻文欣元魂痕寒刪山仙先蕭宵肴豪歌麻陽唐庚耕清青蒸登尤侯幽侵覃談鹽添咸銜嚴凡';
 const 所有聲 = '平上去入';
+const 重紐母 = '幫滂並明見溪羣疑影曉';
+const 重紐韻 = '支脂祭眞仙宵清侵鹽';
 
 function assert(b, s) {
   if (!b) {
@@ -45,7 +47,7 @@ export function validate(母, 呼, 等, 重紐, 韻, 聲) {
     assert(呼.length === 1 && [...所有呼].includes(呼), `Unexpected 呼: ${JSON.stringify(呼)}`);
   }
 
-  if ([...'支脂祭眞仙宵清侵鹽'].includes(韻) && [...'幫滂並明見溪羣疑影曉匣云以'].includes(母)) {
+  if ([...重紐母].includes(母) || [...重紐韻].includes(韻)) {
     assert(重紐.length === 1 && [...所有重紐].includes(重紐), `Unexpected 重紐: ${JSON.stringify(重紐)}`);
   } else {
     assert(重紐 == null, '重紐 should be null');
@@ -112,7 +114,7 @@ export function decode(音韻編碼) {
     呼 = null;
   }
 
-  if (![...'支脂祭眞仙宵清侵鹽'].includes(韻) || ![...'幫滂並明見溪羣疑影曉匣云以'].includes(母)) {
+  if (![...重紐母].includes(母) || ![...重紐韻].includes(韻)) {
     重紐 = null;
   }
 

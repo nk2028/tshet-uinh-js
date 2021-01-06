a7c9ab1352ef38605a4d77a9db774f4dcb8dd68a
sgalal <sgalal.me@outlook.com>
2021-01-06 08:49:48 +0800

    Add babel

--- rollup.config.js
+++ rollup.config.js
@@ -1,8 +1,11 @@
+import babel from '@rollup/plugin-babel';
+
 export default {
   input: 'src/main.js',
   output: {
     file: 'bundle.js',
     format: 'umd',
     name: 'Qieyun',
-  }
+  },
+  plugins: [babel({ babelHelpers: 'bundled' })],
 };

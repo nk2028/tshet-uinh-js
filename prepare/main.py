fd9f39dd96b2779f9c10541240089ef12f50e03b
Ayaka Mikazuki <ayaka@mail.shn.hk>
2021-01-19 11:45:16 +0800

    Update tests and documentation

--- prepare/main.py
+++ prepare/main.py
@@ -12,6 +12,12 @@ with open('prepare/data.csv') as f:
 	for line in f:
 		描述, 反切, 字頭, 解釋 = line.rstrip('\n').split(',')
 
+		# patch
+		if 反切 in ('姊規', '扶來', '昨閑', '莫亥', '莫代', '乙白'):
+			continue # strange 反切, and only corresponds to rare characters
+		if 反切 == '去其' and 字頭 == '抾':
+			continue # 抾，去其切，又丘之切, but the two fanqie imply the same phonological position
+
 		母, 呼, 等, 重紐, 韻, 聲 = from描述(描述)
 		編碼 = to編碼(母, 呼, 等, 重紐, 韻, 聲)
 

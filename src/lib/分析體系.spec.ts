06750df9ae6ead551aba5b235655d473a54bd511
syimyuzya <syimyuzya@gmail.com>
2022-04-03 19:15:18 +0800

    Implement complete(-ish) v2 & v2Strict

--- src/lib/分析體系.spec.ts
+++ src/lib/分析體系.spec.ts
@@ -6,20 +6,39 @@ import { 音韻地位 } from './音韻地位';
 const from = (x: string) => 音韻地位.from描述(x);
 
 test('v2', t => {
-  const conv = (x: string, y: string) => t.is(分析體系.v2(from(x)).描述, from(y).描述);
+  const conv = (x: string, y: string) => {
+    const px = from(x);
+    const py = from(y);
+    t.is(分析體系.v2(px).描述, py.描述);
+    if (!px.等於(py)) {
+      t.throws(() => 分析體系.v2Strict(px), undefined, px.描述);
+    }
+  };
   const rej = (x: string, msg?: string) => t.throws(() => 分析體系.v2(from(x)), msg ? { message: msg } : undefined);
-  // TODO 音韻地位限定條件放寬後測試更多項
 
   conv('幫凡入', '幫凡入');
   conv('定開脂去', '定開脂去');
 
+  // 中立韻、脣音呼
+  conv('端開冬平', '端冬平');
+  conv('端合冬平', '端冬平');
+  conv('疑合虞平', '疑虞平');
+  conv('幫合三東平', '幫三東平');
+
   // 灰咍嚴凡
   conv('並咍上', '並灰上');
+  conv('並開咍上', '並灰上');
   conv('明嚴去', '明凡去');
   conv('見凡去', '見嚴去');
 
+  // 指定開合韻
+  rej('見開文平');
+  rej('見合欣平');
+  rej('幫開凡入');
+  rej('幫合嚴入');
+
   // 清
-  //run('幫A清入', '幫清入');
+  conv('幫A清入', '幫清入');
   conv('幫B清入', '幫三庚入');
 
   // 端知類隔
@@ -38,7 +57,7 @@ test('v2', t => {
   rej('崇開先平');
 
   // 云匣
-  //conv('匣開A眞平', '匣開A眞平');
+  conv('匣開A眞平', '匣開A眞平');
   conv('云灰上', '云合廢上');
   rej('云合山平');
 

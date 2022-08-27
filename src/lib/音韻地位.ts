1ec73bfb83ea7380f93765791bd62347747d0050
syimyuzya <syimyuzya@gmail.com>
2022-08-27 13:26:52 +0800

    Upgrade 編碼
    The new 編碼 scheme is not compatible with v0.13.

--- src/lib/音韻地位.ts
+++ src/lib/音韻地位.ts
@@ -18,9 +18,6 @@ const 所有組 = '幫端知精莊章見影';
 
 const 檢查 = { 母: 所有母, 等: 所有等, 韻: 所有韻, 音: 所有音, 攝: 所有攝, 組: 所有組, 聲: 所有聲 };
 
-// 用於編碼計算，兼容 0.12 編碼（可以等之後可能編碼要升位或大幅重編之類的再停止兼容，不然新舊編碼太相似容易迷惑
-const v1重紐韻 = 重紐韻.concat(['清']);
-
 const 韻順序表 = '東_冬鍾江支脂之微魚虞模齊祭泰佳皆夬灰咍廢眞臻文欣元魂痕寒刪山仙先蕭宵肴豪歌_麻_陽唐庚_耕清青蒸登尤侯幽侵覃談鹽添咸銜嚴凡';
 
 const 一等韻 = '冬模泰咍灰痕魂寒豪唐登侯覃談';
@@ -369,18 +366,16 @@ export class 音韻地位 {
    * 'A9D'
    * > 音韻地位 = Qieyun.音韻地位.from描述('羣開三A支平');
    * > 音韻地位.編碼;
-   * 'fFA'
+   * 'fFU'
    * ```
    */
   get 編碼(): string {
     const { 母, 呼, 等, 重紐, 韻, 聲 } = this;
     const 母編碼 = 所有母.indexOf(母);
     const 韻編碼 = { 東三: 1, 歌三: 38, 麻三: 40, 庚三: 44 }[`${韻}${等}`] || 韻順序表.indexOf(韻);
-
-    const 有額外開合 = 呼 !== null && ([...'幫滂並明'].includes(母) || 開合中立的韻.includes(韻));
-    const 特殊重紐 = 重紐 !== null ? !(重紐母.includes(母) && v1重紐韻.includes(韻)) : 重紐母.includes(母) && 韻 === '清';
-    const 其他編碼 = (+有額外開合 << 5) + (+特殊重紐 << 4) + (+(呼 === '合') << 3) + (+(重紐 === 'B') << 2) + 所有聲.indexOf(聲);
-
+    const 呼編碼 = 所有呼.indexOf(呼) + 1;
+    const 重紐編碼 = 所有重紐.indexOf(重紐) + 1;
+    const 其他編碼 = (呼編碼 << 4) | (重紐編碼 << 2) | 所有聲.indexOf(聲);
     return 編碼表[母編碼] + 編碼表[韻編碼] + 編碼表[其他編碼];
   }
 
@@ -821,32 +816,30 @@ export class 音韻地位 {
    * ```typescript
    * > Qieyun.音韻地位.from編碼('A9D');
    * 音韻地位 { '幫三凡入' }
-   * > Qieyun.音韻地位.from編碼('fFA');
+   * > Qieyun.音韻地位.from編碼('fFU');
    * 音韻地位 { '羣開三A支平' }
    * ```
    */
   static from編碼(音韻編碼: string): 音韻地位 {
-    assert(音韻編碼.length === 3, `Invalid 編碼: ${JSON.stringify(音韻編碼)}`);
+    // NOTE For the 1st code unit, only characters from 'A' through 'l' (inclusive) are valid.
+    // Other characters are reserved for future extension.
+    assert(音韻編碼.length === 3 && 編碼表.slice(0, 38).includes(音韻編碼[0]), `Invalid 編碼: ${JSON.stringify(音韻編碼)}`);
 
     const 母編碼 = 編碼表.indexOf(音韻編碼[0]);
     const 韻編碼 = 編碼表.indexOf(音韻編碼[1]);
     const 其他編碼 = 編碼表.indexOf(音韻編碼[2]);
 
-    const 有額外開合 = (其他編碼 >> 5) & 0b1;
-    const 特殊重紐 = (其他編碼 >> 4) & 0b1;
-    const 呼編碼 = (其他編碼 >> 3) & 0b1;
-    const 重紐編碼 = (其他編碼 >> 2) & 0b1;
+    const 呼編碼 = (其他編碼 >> 4) & 0b11;
+    const 重紐編碼 = (其他編碼 >> 2) & 0b11;
     const 聲編碼 = 其他編碼 & 0b11;
 
     const 母 = 所有母[母編碼];
     const 聲 = 所有聲[聲編碼];
-
-    let 呼 = 所有呼[呼編碼];
-    let 重紐 = 所有重紐[重紐編碼];
+    const 呼 = 呼編碼 ? 所有呼[呼編碼 - 1] : null;
+    const 重紐 = 重紐編碼 ? 所有重紐[重紐編碼 - 1] : null;
 
     let 韻: string;
     let 等: string;
-
     if (韻編碼 in 特別編碼) {
       [韻, 等] = 特別編碼[韻編碼];
     } else {
@@ -855,31 +848,6 @@ export class 音韻地位 {
       if ([...二等韻].includes(韻)) 等 = '二';
       if ([...三等韻].includes(韻)) 等 = '三';
       if ([...四等韻].includes(韻)) 等 = '四';
-    }
-
-    if ([...'幫滂並明'].includes(母) || 開合中立的韻.includes(韻)) {
-      if (!有額外開合) {
-        呼 = null;
-      }
-    } else if (有額外開合) {
-      throw new Error('Invalid code: 呼-flag should be 0');
-    }
-
-    if (重紐母.includes(母) && v1重紐韻.includes(韻)) {
-      if (韻 === '清') {
-        // 清韻鈍音特殊（向上兼容）：flag 表示重紐為 null
-        if (特殊重紐) {
-          重紐 = null;
-        }
-      } else if (特殊重紐) {
-        // 重紐八韻鈍音不允許該 flag
-        throw new Error('Invalid code: 重紐-flag should be 0');
-      }
-    } else {
-      // 其餘情形 flag 表示重紐非 null
-      if (!特殊重紐) {
-        重紐 = null;
-      }
     }
 
     return new 音韻地位(母, 呼, 等, 重紐, 韻, 聲);

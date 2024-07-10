549bac1ceb60864873b98f03b840006cf81d13d7
syimyuzya <syimyuzya@gmail.com>
2024-07-10 16:38:18 +0800

    refactor(obsolete)!: remove obsolete modules
    Modules `分析體系` & `推導方案` removed

--- src/index.ts
+++ src/index.ts
@@ -2,9 +2,4 @@ export { 音韻地位 } from './lib/音韻地位';
 
 export * as 資料 from './lib/解析資料';
 
-export { 適配分析體系 } from './lib/分析體系';
-export type { 適配函數, 適配分析體系選項 } from './lib/分析體系';
-
 export * as 表達式 from './lib/常用表達式';
-
-export * as 推導方案 from './lib/推導方案';

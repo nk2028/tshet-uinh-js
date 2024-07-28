0fa824718a18caea384af56b3ff4fa9549159bdf
syimyuzya <syimyuzya@gmail.com>
2024-07-28 21:57:16 +0800

    feat: bundle .d.ts (again)
    Bundled as `index.d.ts` as before

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,20 +1,31 @@
 // @ts-check
 
 import typescript from '@rollup/plugin-typescript';
+import dts from 'rollup-plugin-dts';
 
-export default {
-  input: 'src/index.ts',
-  output: {
-    file: 'index.js',
-    format: 'umd',
-    sourcemap: true,
-    name: 'Qieyun',
-    exports: 'named',
+export default [
+  {
+    input: 'src/index.ts',
+    output: {
+      file: 'index.js',
+      format: 'umd',
+      sourcemap: true,
+      name: 'Qieyun',
+      exports: 'named',
+    },
+    plugins: [
+      typescript({
+        // NOTE Apparently needed with `"incremental": true` in tsconfig
+        outputToFilesystem: false,
+      }),
+    ],
   },
-  plugins: [
-    typescript({
-      // XXX Apparently needed with `"incremental": true` in tsconfig
-      outputToFilesystem: false,
-    }),
-  ],
-};
+  {
+    input: 'build/esnext/index.d.ts',
+    output: {
+      file: 'index.d.ts',
+      format: 'es',
+    },
+    plugins: [dts()],
+  },
+];

838d6849ef81c206e31bd42549c18ef743a1e544
graphemecluster <graphemecluster@gmail.com>
2024-10-07 20:28:36 +0800

    Output more bundle format

--- rollup.config.js
+++ rollup.config.js
@@ -5,14 +5,28 @@ import typescript from '@rollup/plugin-typescript';
 /** @type { import("rollup").RollupOptions[] } */
 export default [
   {
-    input: 'src/index.ts',
-    output: {
-      file: 'index.js',
-      format: 'umd',
-      sourcemap: true,
-      name: 'TshetUinh',
-      exports: 'named',
-    },
+    input: './src/index.ts',
+    output: [
+      {
+        file: './dist/index.js',
+        format: 'umd',
+        name: 'TshetUinh',
+        exports: 'named',
+        sourcemap: true,
+      },
+      {
+        file: './dist/index.cjs',
+        format: 'cjs',
+        exports: 'named',
+        sourcemap: true,
+      },
+      {
+        file: './dist/index.mjs',
+        format: 'es',
+        exports: 'named',
+        sourcemap: true,
+      },
+    ],
     plugins: [
       typescript({
         // NOTE Apparently needed with `"incremental": true` in tsconfig

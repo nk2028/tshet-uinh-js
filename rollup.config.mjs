bf21c7066f926309b5b81876098537e928e16ef5
syimyuzya <syimyuzya@gmail.com>
2025-03-19 13:10:01 +0800

    build: bundle ESM alongside UMD
    For modern browser usage.

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -5,13 +5,21 @@ import typescript from '@rollup/plugin-typescript';
 export default [
   {
     input: 'src/index.ts',
-    output: {
-      file: 'index.js',
-      format: 'umd',
-      sourcemap: true,
-      name: 'TshetUinh',
-      exports: 'named',
-    },
+    output: [
+      {
+        file: 'dist/tshet-uinh.js',
+        format: 'umd',
+        name: 'TshetUinh',
+        exports: 'named',
+        sourcemap: true,
+      },
+      {
+        file: 'dist/tshet-uinh.esm.js',
+        format: 'es',
+        exports: 'named',
+        sourcemap: true,
+      },
+    ],
     plugins: [
       typescript({
         // NOTE Apparently needed with `"incremental": true` in tsconfig

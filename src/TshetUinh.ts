f75779fcb667ee61b4728484ec800e9c5b425712
Ayaka <ayaka@mail.shn.hk>
2025-07-10 17:00:38 +0100

    Add 反切

--- src/TshetUinh.ts
+++ src/TshetUinh.ts
@@ -8,3 +8,7 @@ export * as 表達式 from './lib/常用表達式';
 export * as 壓縮表示 from './lib/壓縮表示';
 
 export { 韻鏡位置, 音韻地位2韻鏡位置 } from './lib/韻鏡';
+
+export * as 音韻屬性常量 from './lib/音韻屬性常量';
+
+export { 執行反切 } from './lib/反切';

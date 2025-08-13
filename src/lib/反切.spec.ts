ee9d125f075b546982f2c13a902c94106a6fe372
Ayaka <ayaka@mail.shn.hk>
2025-08-13 02:45:50 +0100

    Add 反切 rules

---
+++ src/lib/反切.spec.ts
@@ -0,0 +1,56 @@
+import test from 'ava';
+
+import { defaultLogger } from './StringLogger';
+import { 執行反切 } from './反切';
+import { 音韻地位 } from './音韻地位';
+
+test('可以正常執行反切', t => {
+  const data = [
+    ['東', '德', '紅', '端一東平', '端開一登入', '匣一東平'],
+    ['同', '徒', '紅', '定一東平', '定一模平', '匣一東平'],
+  ];
+
+  let rightCount = 0;
+  let wrongCount = 0;
+
+  for (const [, , , 被切字音韻描述, 上字音韻描述, 下字音韻描述] of data) {
+    const 被切字音韻地位 = 音韻地位.from描述(被切字音韻描述);
+    const 上字音韻地位 = 音韻地位.from描述(上字音韻描述);
+    const 下字音韻地位 = 音韻地位.from描述(下字音韻描述);
+
+    const 預測音韻地位們 = 執行反切(上字音韻地位, 下字音韻地位);
+
+    const hasEqual = 預測音韻地位們.some(預測音韻地位 => 預測音韻地位.等於(被切字音韻地位));
+    if (hasEqual) {
+      rightCount += 1;
+    } else {
+      wrongCount += 1;
+    }
+  }
+
+  const accuracy = rightCount / (rightCount + wrongCount);
+  t.true(accuracy > 0.99, '反切的準確率必須大於 99%');
+});
+
+test('可以為反切結果給出解釋', t => {
+  const data = ['東', '德', '紅', '端一東平', '端開一登入', '匣一東平'];
+  const [, , , 被切字音韻描述, 上字音韻描述, 下字音韻描述] = data;
+
+  const 被切字音韻地位 = 音韻地位.from描述(被切字音韻描述);
+  const 上字音韻地位 = 音韻地位.from描述(上字音韻描述);
+  const 下字音韻地位 = 音韻地位.from描述(下字音韻描述);
+
+  defaultLogger.enable = true;
+  const 預測音韻地位們 = 執行反切(上字音韻地位, 下字音韻地位);
+  const 解釋 = defaultLogger.popAll();
+  defaultLogger.enable = false;
+
+  const hasEqual = 預測音韻地位們.some(預測音韻地位 => 預測音韻地位.等於(被切字音韻地位));
+  t.true(hasEqual, '可以正常反切');
+
+  // console.log(解釋);
+  t.true(
+    解釋[0] === '反切上字為端母，故被切字為端母' && 解釋[1] === '反切下字為東韻平聲，故被切字為東韻平聲',
+    '可以正常為反切結果給出解釋',
+  );
+});
---
+++ src/lib/反切.ts
@@ -0,0 +1,159 @@
+import { defaultLogger } from './StringLogger';
+import { 音韻地位 } from './音韻地位';
+import { 呼韻搭配, 等母搭配, 等韻搭配, 鈍音母 } from './音韻屬性常量';
+
+const 重紐韻 = '支脂祭真仙宵清侵鹽';
+
+export const 執行反切 = (上字音韻地位: 音韻地位, 下字音韻地位: 音韻地位): 音韻地位[] => {
+  const { 母, 組, 呼: 上字呼, 等: 上字等, 類: 上字類 } = 上字音韻地位;
+  defaultLogger.log(`反切上字為${母}母，故被切字為${母}母`);
+
+  const { 韻, 聲, 呼: 下字呼, 組: 下字組, 等: 下字等 } = 下字音韻地位;
+  defaultLogger.log(`反切下字為${韻}韻${聲}聲，故被切字為${韻}韻${聲}聲`);
+
+  let 呼;
+  if (組 === '幫' || 呼韻搭配.中立.includes(韻)) {
+    呼 = null;
+  } else if (呼韻搭配.開.includes(韻)) {
+    呼 = '開';
+    defaultLogger.log(`被切字為${韻}韻，${韻}韻為開口，故被切字為開口`);
+  } else if (呼韻搭配.合.includes(韻)) {
+    呼 = '合';
+    defaultLogger.log(`被切字為${韻}韻，${韻}韻為合口，故被切字為合口`);
+  } else if (母 === '云') {
+    呼 = '合';
+    defaultLogger.log('被切字為云母，云母為合口，故被切字為合口');
+  } else {
+    if (上字呼 === '開' && 下字呼 === '開') {
+      呼 = '開';
+      defaultLogger.log('反切上下字均為開口，故被切字為開口');
+    } else if (下字呼 === '合') {
+      呼 = '合';
+      defaultLogger.log('反切下字為合口，故被切字為合口');
+    } else if (上字呼 === '合' && 下字組 === '幫') {
+      呼 = '合';
+      defaultLogger.log('反切上字為合口，下字為幫組，故被切字為合口');
+    } else {
+      呼 = '開合';
+      defaultLogger.log('無法確定被切字的呼，可能為開口或合口');
+    }
+  }
+
+  let 等;
+  if (等韻搭配.一.includes(韻)) {
+    等 = '一';
+    defaultLogger.log(`被切字為${韻}韻，${韻}韻為一等，故被切字為一等`);
+  } else if (等韻搭配.二.includes(韻)) {
+    等 = '二';
+    defaultLogger.log(`被切字為${韻}韻，${韻}韻為二等，故被切字為二等`);
+  } else if (等韻搭配.三.includes(韻)) {
+    等 = '三';
+    defaultLogger.log(`被切字為${韻}韻，${韻}韻為三等，故被切字為三等`);
+  } else if (等韻搭配.四.includes(韻)) {
+    等 = '四';
+    defaultLogger.log(`被切字為${韻}韻，${韻}韻為四等，故被切字為四等`);
+  } else if (下字等 === '三') {
+    等 = '三';
+    defaultLogger.log('反切下字為三等，故被切字為三等');
+  } else if (上字等 !== '三' && 下字等 !== '三') {
+    defaultLogger.log('反切上下字均非三等，故被切字非三等');
+    if (等韻搭配.一三.includes(韻)) {
+      等 = '一';
+      defaultLogger.log(`被切字為${韻}韻，${韻}韻為一等或三等，而被切字非三等，故被切字為一等`);
+    } else if (等韻搭配.二三.includes(韻)) {
+      等 = '二';
+      defaultLogger.log(`被切字為${韻}韻，${韻}韻為二等或三等，而被切字非三等，故被切字為二等`);
+    } else {
+      throw Error('Unreachable');
+    }
+  } else {
+    if (等韻搭配.一三.includes(韻)) {
+      defaultLogger.log(`被切字為${韻}韻，${韻}韻為一等或三等，故被切字為一等或三等`);
+      if (等母搭配.二三.includes(母) || 等母搭配.三.includes(母)) {
+        等 = '三';
+        defaultLogger.log(`被切字為${母}母，${母}母不可能為一等，故被切字為三等`);
+      } else if (等母搭配.一二四.includes(母)) {
+        defaultLogger.log(`被切字為${母}母，${母}母不可能為三等，故被切字為一等`);
+        等 = '一';
+      } else {
+        等 = '一'; // 實際為一、三，此處僅選取第一個
+      }
+    } else if (等韻搭配.二三.includes(韻)) {
+      defaultLogger.log(`被切字為${韻}韻，${韻}韻為二等或三等，故被切字為二等或三等`);
+      if (等母搭配.一三四.includes(母) || 等母搭配.三.includes(母)) {
+        等 = '三';
+        defaultLogger.log(`被切字為${母}母，${母}母不可能為二等，故被切字為三等`);
+      } else if (等母搭配.一二四.includes(母)) {
+        等 = '二'; // 不可能為二等
+        defaultLogger.log(`被切字為${母}母，${母}母不可能為三等，故被切字為二等`);
+      } else {
+        等 = '二'; // TODO: 實際為二、三，此處僅選取第一個
+        defaultLogger.log('無法確定被切字的等，可能為二等或三等');
+      }
+    } else {
+      throw Error('Unreachable');
+    }
+  }
+
+  let 類;
+  if (等 !== '三' || !鈍音母.includes(母)) {
+    類 = null;
+  } else if (韻 === '幽') {
+    if (組 === '幫') {
+      類 = 'B'; // 幫組、「惆」、「烋」爲 B 類
+      defaultLogger.log('被切字為幽韻，且為幫組，故被切字為 B 類');
+    } else {
+      類 = 'A';
+      defaultLogger.log('被切字為幽韻，且非幫組，故被切字為 A 類');
+    }
+  } else if (韻 === '蒸') {
+    if (組 === '幫' || 呼 === '合') {
+      類 = 'B'; // 幫組、合口、「抑」爲 B 類
+      defaultLogger.log('被切字為蒸韻，且為幫組或合口，故被切字為 B 類');
+    } else {
+      類 = 'C';
+      defaultLogger.log('被切字為蒸韻，且非幫組或合口，故被切字為 C 類');
+    }
+  } else if (韻 === '庚') {
+    類 = 'B';
+    defaultLogger.log('被切字為庚韻，故被切字為 B 類');
+  } else if (!重紐韻.includes(韻)) {
+    類 = 'C';
+    defaultLogger.log('被切字非重紐韻，故被切字為 C 類'); // TODO: confirm this
+  } else if (母 === '云') {
+    類 = 'B';
+    defaultLogger.log('被切字為云母，故被切字為 B 類');
+  } else {
+    if (上字類 === 'A') {
+      類 = 'A';
+      defaultLogger.log('反切上字為 A 類，故被切字為 A 類');
+    } else if (上字類 === 'B') {
+      類 = 'B';
+      defaultLogger.log('反切上字為 B 類，故被切字為 B 類');
+    } else if (下字音韻地位.屬於('A類 或 以母 或 精組')) {
+      類 = 'A';
+      defaultLogger.log('反切下字為 A 類、以母或精組，故被切字為 A 類');
+    } else if (下字音韻地位.屬於('B類 或 云母')) {
+      類 = 'B';
+      defaultLogger.log('反切下字為 B 類或云母，故被切字為 B 類');
+    } else {
+      類 = 'AB';
+      defaultLogger.log('無法確定被切字的類，可能為 A 類或 B 類');
+    }
+  }
+
+  const res = [];
+  for (const 呼_ of 呼 === '開合' ? [...呼] : [呼]) {
+    for (const 類_ of 類 === 'AB' ? [...類] : [類]) {
+      try {
+        res.push(new 音韻地位(母, 呼_, 等, 類_, 韻, 聲));
+      } catch (e) {
+        void e;
+        // throw e;
+        // return null;
+        void 0;
+      }
+    }
+  }
+  return res;
+};

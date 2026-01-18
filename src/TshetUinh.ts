1a9af8f41b64dbc6a2bf09c8b1fe0f4bbdcbb2b3
Ayaka <ayaka@mail.shn.hk>
2026-01-18 19:57:17 +0100

    Remove 韻鏡 module

--- src/TshetUinh.ts
+++ src/TshetUinh.ts
@@ -7,8 +7,6 @@ export * as 表達式 from './lib/常用表達式';
 
 export * as 壓縮表示 from './lib/壓縮表示';
 
-export { 韻鏡位置, 音韻地位2韻鏡位置 } from './lib/韻鏡';
-
 export { 執行反切 } from './lib/反切';
 
 export { StringLogger, defaultLogger } from './lib/StringLogger';
